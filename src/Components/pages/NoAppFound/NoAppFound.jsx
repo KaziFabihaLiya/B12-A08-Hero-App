99e31f83f1d4ae2e622595722856fafe72e5e65b
Kazi Fabiha Golam Liya <151756377+KaziFabihaLiya@users.noreply.github.com>
2025-10-11 22:21:19 +0600

    text changed

--- src/Components/pages/NoAppFound/NoAppFound.jsx
+++ src/Components/pages/NoAppFound/NoAppFound.jsx
@@ -7,14 +7,14 @@ const NoAppFound = () => {
     const handleGoBack = () => {
         navigate(-1);
     };
-    
+
     return (
         <div>
             <div>
                 <img className='mx-auto w-2xl m-10' src="/App-Error.png" alt="" />
                 <div className='mx-20 mt-10 mb-10 text-center justify-center'>
-                    <h2 className='text-5xl font-bold'>Oops, page not found!</h2>
-                    <p className='text-xl text-[#627382] font-normal mt-4'>The page you are looking for is not available.</p>
+                    <h2 className='text-5xl font-bold'>OOPS, APP NOT FOUND</h2>
+                    <p className='text-xl text-[#627382] font-normal mt-4'>The App you are requesting is not found on our system.  please try another apps</p>
                 <div className='mt-10 mb-20 justify-center flex'>
                     <button onClick={()=> handleGoBack()} className="btn bg-gradient-to-r from-[#632EE3] to-[#9F62F2] text-white ">Go Back!</button>
                 </div>
